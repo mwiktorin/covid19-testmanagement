4f14f85c6911266dd0eecb94e498f6f1f6b0a40b
mottlik <mottlik214@gmail.com>
2020-03-21 21:03:09 +0100

    fix request cancelled

--- ui/src/testzentrum/register-testzentrum.comp.tsx
+++ ui/src/testzentrum/register-testzentrum.comp.tsx
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { TestzentrumDto } from './testzentrum.dto';
 import { InputText } from 'primereact/inputtext';
 import { TestzentrumService } from './testzentrum.service';
 import './register-testzentrum.css';
 import { Button } from 'primereact/button';
+import { Growl } from 'primereact/growl';
 
 export interface RegisterTestzentrumProps {
     testzentrumService: TestzentrumService
 }
 
 export const RegisterTestzentrum = (props: RegisterTestzentrumProps) => {
-    const [name, setName] = React.useState("");
-    const [plz, setPlz] = React.useState("");
-    const [anschrift, setAnschrift] = React.useState("");
-    const [telefonnummer, setTelefonnummer] = React.useState("");
+    const [name, setName] = useState("");
+    const [plz, setPlz] = useState("");
+    const [anschrift, setAnschrift] = useState("");
+    const [telefonnummer, setTelefonnummer] = useState("");
+    const [growl, setGrowl] = useState();
 
-    const handleSubmit = () => props.testzentrumService.addNewTestzentrum(new TestzentrumDto(name, plz, anschrift, telefonnummer));
+    const handleSubmit = () =>
+        props.testzentrumService.addNewTestzentrum(new TestzentrumDto(name, plz, anschrift, telefonnummer))
+            .then(() => growl.show({severity: 'success', summary: 'Anfrage erfolgreich'}))
+            .catch(() => growl.show({severity: 'error', summary: 'Anfrage fehlgeschlagen'}));
 
     return (
         <>
             <h1>Registrierung</h1>
-            <form className={"center-vertically"} onSubmit={() => handleSubmit()}>
-                <label>
-                    Name der Station
+            <form className={"center-vertically"} onSubmit={event => {
+                event.preventDefault();
+                handleSubmit();
+            }}>
+                <div className={"d-flex"}>
+                    <label>Name der Station</label>
                     <InputText onChange={(e) => setName(e.currentTarget.value)}/>
-                </label>
-                <label>
-                    PLZ
+                </div>
+                <div>
+                    <label>PLZ</label>
                     <InputText onChange={(e) => setPlz(e.currentTarget.value)}/>
-                </label>
-                <label>
-                    Straße/Nr.
+                </div>
+                <div>
+                    <label>Straße/Nr.</label>
                     <InputText onChange={(e) => setAnschrift(e.currentTarget.value)}/>
-                </label>
-                <label>
-                    Telefonnummer
+                </div>
+                <div>
+                    <label>Telefonnummer</label>
                     <InputText onChange={(e) => setTelefonnummer(e.currentTarget.value)}/>
-                </label>
-                <Button type="submit" label={"Registrieren"}/>
+                </div>
+                <Button label={"Registrieren"} type={"submit"}/>
             </form>
+            <Growl ref={(el) => setGrowl(el)}/>
         </>
     );
 };
