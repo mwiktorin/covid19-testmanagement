f7887b186df4a91e9778fe4c44ae03c46a1052c0
mottlik <mottlik214@gmail.com>
2020-03-22 11:40:45 +0100

    unstable verfuegbarkeit testzentrum

--- ui/src/testzentrum/testzentrum.service.ts
+++ ui/src/testzentrum/testzentrum.service.ts
@@ -1,5 +1,6 @@
 import { HttpService } from '../service/http.service';
 import { TestzentrumDto } from './testzentrum.dto';
+import { VerfuebarkeitTestzentrumDto } from './verfuebarkeit-testzentrum.dto';
 
 export class TestzentrumService {
     constructor(private readonly httpService: HttpService = new HttpService()) {
@@ -8,5 +9,9 @@ export class TestzentrumService {
     async addNewTestzentrum(testzentrumDto: TestzentrumDto) {
         const response = await this.httpService.post('api/testzentrum', testzentrumDto);
         return response.data as TestzentrumDto;
+    }
+
+    async addVerfuegbarkeitTestzentrum(testzentrumId: string, verfuebarkeitTestzentrumDto: VerfuebarkeitTestzentrumDto): Promise<void> {
+        await this.httpService.put(`api/testzentrum/${testzentrumId}`, verfuebarkeitTestzentrumDto);
     }
 }
