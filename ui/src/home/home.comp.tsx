f52d8b5a47a5c74cbd45efda16a38e0784a36e21
Markus Wiktorin <markuswiktorin@gmail.com>
2020-03-22 17:26:26 +0100

    update home text

--- ui/src/home/home.comp.tsx
+++ ui/src/home/home.comp.tsx
@@ -5,15 +5,15 @@ export const Home = () => (
     <>
         <h1>GetMeTested</h1>
         <p>
-            GetMeTested ist eine Plattform die einen geordneten, übersichtlichen, effizienten, effektiven und
-            transparenten Ablauf im Rahmen des SARS-CoV2 Tests gewährleistet. Die Platform dient in erster Linie den
-            Patienten, den Gesundheitsämter den Laboren und der Kassenärztlichen Vereinigungen und hilft bei der
-            Koordination von Terminen und der Übermittlung des individuellem persönlichen Testergebnisses.
+            GetMeTested ist eine Plattform, die einen geordneten, übersichtlichen, effizienten, effektiven und
+            transparenten Ablauf im Rahmen des SARS-CoV2 Tests gewährleistet. Die Plattform dient in erster Linie den
+            Patienten, den Gesundheitsämtern, den Laboren und den Kassenärztlichen Vereinigungen und hilft bei der
+            Koordination von Terminen und der Übermittlung des individuellen persönlichen Testergebnisses.
         </p>
         <p>
-            Wir helfen Dir einen Termin bei deiner nächsten Teststation auszumachen und Dein Testergebnis so schnell es
-            geht einzusehen! Zur Erleichterung der Vorauswahl verwende bitte zunächst die COV-App. Der Fragebogen dient
-            dazu festzustellen, ob eine weitere Testung nötig ist.
+            Wir helfen Dir, einen Termin bei einem Testzentrum in Deiner Nähe zu bekommen und Dein Testergebnis
+            schnellstmöglich einzusehen! Zur Erleichterung der Vorauswahl verwende bitte zunächst die CovApp. Der
+            Fragebogen dient dazu festzustellen, ob ein Test nötig ist.
         </p>
         <div style={{display: "flex", textAlign: "center", flexDirection: "column"}}>
             <p>
